0c409270c94aa6d760b9d67eef029fc7afb9fdff
SMAnish-28 <anish.coder.genius@gmail.com>
2024-06-14 20:15:22 +0530

    before routing

--- piyush_garg/index.js
+++ piyush_garg/index.js
@@ -4,8 +4,39 @@ const app = express();
 const PORT = 8000;
 
 // Routes
-app.get('/users', (req, res) => {
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>
+      ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+  `;
+  res.send(html);
+});
+
+// REST API
+app.get('/api/users', (req, res) => {
   return res.json(users);
+});
+
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
+});
+
+app.post("/api/users", (req,res) => {
+  // TODO: Create new user
+  return res.json({status: "pending"});
+});
+
+app.patch("/api/users/:id", (req,res) => {
+  // TODO: Edit the user with id
+  return res.json({status: "pending"});
+});
+
+app.delete("/api/users/:id", (req,res) => {
+  // TODO: Delete the user with id
+  return res.json({status: "pending"});
 });
 
 app.listen(PORT, () => {

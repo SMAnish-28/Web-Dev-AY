0bafc414ee6503789801aec19ad07829fbef9ab7
SMAnish-28 <anish.coder.genius@gmail.com>
2024-06-14 21:43:29 +0530

    before postman

--- piyush_garg/index.js
+++ piyush_garg/index.js
@@ -18,24 +18,24 @@ app.get('/api/users', (req, res) => {
   return res.json(users);
 });
 
-app.get("/api/users/:id", (req, res) => {
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
   const id = Number(req.params.id);
   const user = users.find((user) => user.id === id);
   return res.json(user);
-});
+  })
+  .patch((req, res) => {
+  // TODO: Edit the user with id
+  return res.json({status: "pending"});
+  })  
+  .delete((req, res) => {
+  // TODO: Delete the user with id
+  return res.json({status: "pending"});
+  })
 
 app.post("/api/users", (req,res) => {
   // TODO: Create new user
-  return res.json({status: "pending"});
-});
-
-app.patch("/api/users/:id", (req,res) => {
-  // TODO: Edit the user with id
-  return res.json({status: "pending"});
-});
-
-app.delete("/api/users/:id", (req,res) => {
-  // TODO: Delete the user with id
   return res.json({status: "pending"});
 });
 
